e55610e05cf1902e4fcdbcc22820dd6735262835
ZeruKakin <Zerukakin@gmail.com>
2023-03-10 11:14:10 -0800

    lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void _puts(char *s)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
