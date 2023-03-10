2563bd80185ce1ad177cdc6049fc792260a2f9a9
ZeruKakin <Zerukakin@gmail.com>
2023-03-10 13:16:11 -0800

    4-add.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -51,37 +51,20 @@ int _atoi(char *s)
 		{
 
 			digit = s[i] - '0';
-
 			if (d % 2)
-
-				digit = -digit;
-
+			digit = -digit;
 			n = n * 10 + digit;
-
 			f = 1;
-
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-
 				break;
-
 			f = 0;
-
 		}
-
 		i++;
-
 	}
-
-
-
-	if (f == 0)
-
-		return (0);
-
-
+if (f == 0)
+return (0);
 
 	return (n);
-
 }
 
 
